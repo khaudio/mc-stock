fa3819b29f93334a384d3a3c0a2018f8108ea62f
Kyle Hughes <kylehughes1@gmail.com>
2018-02-03 11:46:25 -0600

    initial commit; added old script and new empty main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+#/usr/bin/env python
+
+"""
+mc-stock checks stock on specified items at Microcenter store locations, and sends email notifications when changes are detected.  Applicably, it helps the user obtain rare items during shortages, like graphics cards.
+"""
+
+
+from time import sleep
+import pickle
+import re
+import requests
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
